5ae71cd839bc2a3ea537592b330e18794c5d211c
Jan Martin Keil <jan-martin.keil@uni-jena.de>
2021-07-21 16:42:12 +0200

    docs

--- generateDocs.sh
+++ generateDocs.sh
@@ -1,8 +1,8 @@
 # download widoco.jar, if not present
 if [ ! -f "widoco.jar" ] 
 then
-    wget -q -O widoco.jar https://github.com/dgarijo/Widoco/releases/download/v1.4.14/widoco-1.4.14-jar-with-dependencies.jar
+    wget -q -O widoco.jar https://github.com/dgarijo/Widoco/releases/download/v1.4.15/widoco-1.4.15-jar-with-dependencies.jar
 fi 
 
 # run WIDOCO (see https://github.com/dgarijo/Widoco#how-to-use-widoco )
-java -jar widoco.jar -uniteSections -rewriteAll -getOntologyMetadata -outFolder docu -ontFile abecto-vocabulary.ttl
+java -jar widoco.jar -uniteSections -rewriteAll -getOntologyMetadata -includeAnnotationProperties -excludeIntroduction -htaccess -outFolder docs -ontFile abecto-vocabulary.ttl
