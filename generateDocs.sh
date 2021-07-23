82563e1b855e66855c2ae6caddcad0b03a1adc8c
Jan Martin Keil <jan-martin.keil@uni-jena.de>
2021-07-23 09:00:54 +0000

    Create CI pipeline

--- generateDocs.sh
+++ generateDocs.sh
@@ -1,8 +1,8 @@
 # download widoco.jar, if not present
 if [ ! -f "widoco.jar" ] 
 then
-    wget -q -O widoco.jar https://github.com/dgarijo/Widoco/releases/download/v1.4.15/widoco-1.4.15-jar-with-dependencies.jar
-fi 
+    curl -s -L -o widoco.jar https://github.com/dgarijo/Widoco/releases/download/v1.4.15/widoco-1.4.15-jar-with-dependencies.jar
+fi
 
 # run WIDOCO (see https://github.com/dgarijo/Widoco#how-to-use-widoco )
 java -jar widoco.jar -uniteSections -rewriteAll -getOntologyMetadata -excludeIntroduction -htaccess -outFolder docs -ontFile abecto-vocabulary.ttl
