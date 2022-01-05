d7be8ee85db065e870f16515a58627f853fda235
Jan Martin Keil <jan-martin.keil@uni-jena.de>
2022-01-05 17:12:57 +0100

    add index.html symlink

--- generateDocs.sh
+++ generateDocs.sh
@@ -19,12 +19,14 @@ java -jar widoco.jar -uniteSections -rewriteAll -getOntologyMetadata -excludeInt
 # cleanup
 rm abecto-vocabulary-without-imports.ttl
 rm "docs/$version/readme.md"
+rm "docs/$version/readme.md"
 
 # remove description and references section (http://austinmatzko.com/2008/04/26/sed-multi-line-search-and-replace/)
 sed -i -n '1h;1!H;${;g;s/<!--DESCRIPTION SECTION-->.*<!--CROSSREF SECTION-->/<!--CROSSREF SECTION-->/g;p;}' "docs/$version/index-en.html"
 sed -i -n '1h;1!H;${;g;s/<div id="references">.*<div id="acknowledgments">/<div id="acknowledgments">/g;p;}' "docs/$version/index-en.html"
 
 # symlinks for current version
+ln -frs "docs/$version/index-en.html" "docs/index.html"
 ln -frs "docs/$version/index-en.html" "docs/index-en.html"
 ln -frs "docs/$version/ontology.jsonld" "docs/ontology.jsonld"
 ln -frs "docs/$version/ontology.nt" "docs/ontology.nt"
