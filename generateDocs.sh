c8bf7272275d9386f854a44a09d13d793270018e
Jan Martin Keil <jan-martin.keil@uni-jena.de>
2022-05-09 14:51:33 +0200

    use pylode instead of widoco

--- generateDocs.sh
+++ generateDocs.sh
@@ -1,43 +1,28 @@
 #!/bin/bash
 
-# download widoco.jar, if not present
-if [ ! -f "widoco.jar" ] 
-then
-    curl -s -L -o widoco.jar https://github.com/dgarijo/Widoco/releases/download/v1.4.15/widoco-1.4.15-jar-with-dependencies.jar
-fi
+pip3 install pylode
 
 # get version number form versionIRI
 version="$(grep -Po -e "(?<=owl:versionIRI\s<http:\/\/w3id\.org\/abecto\/vocabulary\/).*(?=>)" abecto-vocabulary.ttl)"
 echo "$version"
 
-# create version without owl:import statements to avoid inclusion of other terms
-grep -v -e "owl:import" abecto-vocabulary.ttl > abecto-vocabulary-without-imports.ttl
+mkdir "docs/$version"
+pylode -o "docs/$version/index" abecto-vocabulary.ttl
+cp abecto-vocabulary.ttl "docs/$version/abecto-vocabulary.ttl"
 
-# run WIDOCO (see https://github.com/dgarijo/Widoco#how-to-use-widoco )
-java -jar widoco.jar -uniteSections -rewriteAll -getOntologyMetadata -excludeIntroduction -outFolder "docs/$version" -ontFile abecto-vocabulary-without-imports.ttl
-
-# create rdf file with imports
-#java -jar widoco.jar -uniteSections -rewriteAll -getOntologyMetadata -excludeIntroduction -outFolder "docs/$version-with-imports" -ontFile abecto-vocabulary.ttl
-#mv -f "docs/$version-with-imports/ontology.jsonld" "docs/$version"
-#mv -f "docs/$version-with-imports/ontology.nt" "docs/$version"
-#mv -f "docs/$version-with-imports/ontology.rdf" "docs/$version"
-#mv -f "docs/$version-with-imports/ontology.ttl" "docs/$version"
-
-# cleanup
-#rm -rf "docs/$version-with-imports"
-rm abecto-vocabulary-without-imports.ttl
-rm "docs/$version/readme.md"
-
-# remove description and references section (http://austinmatzko.com/2008/04/26/sed-multi-line-search-and-replace/)
-sed -i -n '1h;1!H;${;g;s/<!--DESCRIPTION SECTION-->.*<!--CROSSREF SECTION-->/<!--CROSSREF SECTION-->/g;p;}' "docs/$version/index-en.html"
-sed -i -n '1h;1!H;${;g;s/<div id="references">.*<div id="acknowledgments">/<div id="acknowledgments">/g;p;}' "docs/$version/index-en.html"
+# generate other RDF formats
+python3 -c "import rdflib
+rdflib.Graph().parse('abecto-vocabulary.ttl', format='turtle').serialize('docs/$version/abecto-vocabulary.jsonld', format='json-ld')"
+python3 -c "import rdflib
+rdflib.Graph().parse('abecto-vocabulary.ttl', format='turtle').serialize('docs/$version/abecto-vocabulary.nt', format='nt')"
+python3 -c "import rdflib
+rdflib.Graph().parse('abecto-vocabulary.ttl', format='turtle').serialize('docs/$version/abecto-vocabulary.ttl', format='turtle')"
+python3 -c "import rdflib
+rdflib.Graph().parse('abecto-vocabulary.ttl', format='turtle').serialize('docs/$version/abecto-vocabulary.rdf', format='pretty-xml')"
 
 # symlinks for current version
-ln -frs "docs/$version/index-en.html" "docs/index.html"
-ln -frs "docs/$version/index-en.html" "docs/index-en.html"
-ln -frs "docs/$version/ontology.jsonld" "docs"
-ln -frs "docs/$version/ontology.nt" "docs"
-ln -frs "docs/$version/ontology.rdf" "docs"
-ln -frs "docs/$version/ontology.ttl" "docs"
-ln -frs "docs/$version/provenance" "docs"
-ln -frs "docs/$version/resources" "docs"
+ln -frs "docs/$version/index.html" "docs"
+ln -frs "docs/$version/abecto-vocabulary.jsonld" "docs"
+ln -frs "docs/$version/abecto-vocabulary.nt" "docs"
+ln -frs "docs/$version/abecto-vocabulary.ttl" "docs"
+ln -frs "docs/$version/abecto-vocabulary.rdf" "docs"
