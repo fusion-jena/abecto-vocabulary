9b03e6e6452d03869b17983bbcdca80a7d1f9898
Jan Martin Keil <jan-martin.keil@uni-jena.de>
2023-07-03 23:16:01 +0200

    update doc generation script

--- generateDocs.sh
+++ generateDocs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip3 install pylode
+# requires `pip install pylode`
 
 # get version number form versionIRI
 version="$(grep -Po -e "(?<=owl:versionIRI\s<http:\/\/w3id\.org\/abecto\/vocabulary\/).*(?=>)" abecto-vocabulary.ttl)"
@@ -11,18 +11,18 @@ pylode -o "docs/$version/index" abecto-vocabulary.ttl
 cp abecto-vocabulary.ttl "docs/$version/abecto-vocabulary.ttl"
 
 # generate other RDF formats
-python3 -c "import rdflib
+python -c "import rdflib
 rdflib.Graph().parse('abecto-vocabulary.ttl', format='turtle').serialize('docs/$version/abecto-vocabulary.jsonld', format='json-ld')"
-python3 -c "import rdflib
+python -c "import rdflib
 rdflib.Graph().parse('abecto-vocabulary.ttl', format='turtle').serialize('docs/$version/abecto-vocabulary.nt', format='nt')"
-python3 -c "import rdflib
+python -c "import rdflib
 rdflib.Graph().parse('abecto-vocabulary.ttl', format='turtle').serialize('docs/$version/abecto-vocabulary.ttl', format='turtle')"
-python3 -c "import rdflib
+python -c "import rdflib
 rdflib.Graph().parse('abecto-vocabulary.ttl', format='turtle').serialize('docs/$version/abecto-vocabulary.rdf', format='pretty-xml')"
 
 # symlinks for current version
-ln -frs "docs/$version/index.html" "docs"
-ln -frs "docs/$version/abecto-vocabulary.jsonld" "docs"
-ln -frs "docs/$version/abecto-vocabulary.nt" "docs"
-ln -frs "docs/$version/abecto-vocabulary.ttl" "docs"
-ln -frs "docs/$version/abecto-vocabulary.rdf" "docs"
+cp "docs/$version/index.html" "docs/index.html"
+cp "docs/$version/abecto-vocabulary.jsonld" "docs/abecto-vocabulary.jsonld"
+cp "docs/$version/abecto-vocabulary.nt" "docs/abecto-vocabulary.nt"
+cp "docs/$version/abecto-vocabulary.ttl" "docs/abecto-vocabulary.ttl"
+cp "docs/$version/abecto-vocabulary.rdf" "docs/abecto-vocabulary.rdf"
